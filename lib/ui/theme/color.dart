a581a8dadc3338f3e91f7c9ea32412f734390e39
Muhammad Irvan <mirvan3107@gmail.com>
2021-10-08 13:25:07 +0700

    first commit

---
+++ lib/ui/theme/color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+class MainColor {
+  static Color primaryColor = const Color(0xFF00061A);
+  static Color secondaryColor = const Color(0xFF001456);
+  static Color accentColor = const Color(0xFF4169E8);
+}
